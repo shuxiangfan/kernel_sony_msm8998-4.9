a7019ef87948ee88551d29d37f240f8da647df4d
Sharako <linclonfan@outlook.com>
2021-04-23 19:50:34 +0800

    Add build script
    Signed-off-by: Sharako <linclonfan@outlook.com>

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+args="-j$(nproc --all) \
+	O=out \
+	ARCH=arm64 \
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=${HOME}/cbl/bin/aarch64-linux-gnu- \
+	CC=${HOME}/cbl/bin/clang \
+	CROSS_COMPILE_ARM32=${HOME}/cbl/bin/arm-linux-gnueabi- "
+	make ${args} lineage-msm8998-yoshino-maple_defconfig
+	make ${args}
