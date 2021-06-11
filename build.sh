d489d5f60c91d7755484e5e6a42752e2b4dfc494
Sharakovia <linclonfan@outlook.com>
2021-06-11 19:11:17 +0800

    Revert "build.sh: Use GCC to compile kernel"
    This reverts commit cd2957960f3ed575fb2c7a0f9e51a3c05a683489.
    
    Signed-off-by: Sharakovia <linclonfan@outlook.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 args="-j$(nproc --all) \
 	O=out \
 	ARCH=arm64 \
-	CROSS_COMPILE=${HOME}/gcc49_arm64/bin/aarch64-linux-android- \
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=${HOME}/cbl/bin/aarch64-linux-gnu- \
+	CC=${HOME}/cbl/bin/clang \
 	CROSS_COMPILE_ARM32=${HOME}/cbl/bin/arm-linux-gnueabi- "
 	make ${args} lineage-msm8998-yoshino-maple_defconfig
 	make ${args}
